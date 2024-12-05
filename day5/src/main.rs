39a3ff202aef4752569d370078bd787de912dc8c
Jorik <vanveenjorik@protonmail.com>
2024-12-05 11:22:31 +0100

    Day 5: part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-fn part1() {
+fn get_input() -> (Vec<(usize, usize)>, Vec<Vec<usize>>) {
     let input = std::fs::read_to_string("input").unwrap();
     let mut split = input.split("\n\n");
     let rules_input = split.next().unwrap();
@@ -27,42 +27,143 @@ fn part1() {
         })
         .collect();
 
-    let after_map: HashMap<usize, HashSet<usize>> = rules
-        .iter()
-        .fold(HashMap::new(), |mut map, (before, after)| {
-            match map.get_mut(before) {
-                Some(afters) => { afters.insert(*after); },
-                None => { map.insert(*before, HashSet::from([*after])); },
-            }
-            map
-        });
-    
-    let correctly_ordered: Vec<Vec<usize>> = orderings.iter().filter(|ordering| {
-        let mut seen: HashSet<usize> = HashSet::new();
-        for i in 0..ordering.len() {
-            let afters = match after_map.get(&ordering[i]) {
-                Some(afters) => afters,
-                None => {
-                    seen.insert(ordering[i]); 
-                    continue
-                },
-            };
-
-            if afters.intersection(&seen).count() != 0 {
-                return false
-            }
-
-            seen.insert(ordering[i]); 
-        }
-        true
-    }).cloned().collect();
-
-    let middles_sum: usize = correctly_ordered.iter().map(|ordering| ordering[ordering.len() / 2]).sum();
-
-    dbg!(rules, orderings, after_map, correctly_ordered, middles_sum);
+    (rules, orderings)
 }
 
-fn part2() {}
+fn part1() {
+    let (rules, orderings) = get_input();
+
+    let after_map: HashMap<usize, HashSet<usize>> =
+        rules
+            .iter()
+            .fold(HashMap::new(), |mut map, (before, after)| {
+                match map.get_mut(before) {
+                    Some(afters) => {
+                        afters.insert(*after);
+                    }
+                    None => {
+                        map.insert(*before, HashSet::from([*after]));
+                    }
+                }
+                map
+            });
+
+    let correctly_ordered: Vec<Vec<usize>> = orderings
+        .iter()
+        .filter(|ordering| {
+            let mut seen: HashSet<usize> = HashSet::new();
+            for i in 0..ordering.len() {
+                let afters = match after_map.get(&ordering[i]) {
+                    Some(afters) => afters,
+                    None => {
+                        seen.insert(ordering[i]);
+                        continue;
+                    }
+                };
+
+                if afters.intersection(&seen).count() != 0 {
+                    return false;
+                }
+
+                seen.insert(ordering[i]);
+            }
+            true
+        })
+        .cloned()
+        .collect();
+
+    let middles_sum: usize = correctly_ordered
+        .iter()
+        .map(|ordering| ordering[ordering.len() / 2])
+        .sum();
+
+    dbg!(middles_sum);
+}
+
+fn part2() {
+    let (rules, orderings) = get_input();
+
+    let after_map: HashMap<usize, HashSet<usize>> =
+        rules
+            .iter()
+            .fold(HashMap::new(), |mut map, (before, after)| {
+                match map.get_mut(before) {
+                    Some(afters) => {
+                        afters.insert(*after);
+                    }
+                    None => {
+                        map.insert(*before, HashSet::from([*after]));
+                    }
+                }
+                map
+            });
+
+    let incorrectly_ordered: Vec<Vec<usize>> = orderings
+        .iter()
+        .filter(|ordering| {
+            let mut seen: HashSet<usize> = HashSet::new();
+            for i in 0..ordering.len() {
+                let afters = match after_map.get(&ordering[i]) {
+                    Some(afters) => afters,
+                    None => {
+                        seen.insert(ordering[i]);
+                        continue;
+                    }
+                };
+
+                if afters.intersection(&seen).count() != 0 {
+                    return true;
+                }
+
+                seen.insert(ordering[i]);
+            }
+            false
+        })
+        .cloned()
+        .collect();
+
+    let correctly_ordered: Vec<Vec<usize>> = incorrectly_ordered
+        .into_iter()
+        .map(|mut ordering| {
+            // Basically bubble sort
+            loop {
+                let mut seen: HashSet<usize> = HashSet::new();
+                let mut i_toswap: usize = 0;
+                let mut should_swap = false;
+                for i in 0..ordering.len() {
+                    let afters = match after_map.get(&ordering[i]) {
+                        Some(afters) => afters,
+                        None => {
+                            seen.insert(ordering[i]);
+                            continue;
+                        }
+                    };
+
+                    if afters.intersection(&seen).count() != 0 {
+                        i_toswap = i;
+                        should_swap = true;
+                        break;
+                    }
+
+                    seen.insert(ordering[i]);
+                }
+
+                if !should_swap {
+                    break;
+                }
+
+                ordering.swap(i_toswap, i_toswap - 1);
+            }
+            ordering
+        })
+        .collect();
+
+    let middles_sum: usize = correctly_ordered
+        .iter()
+        .map(|ordering| ordering[ordering.len() / 2])
+        .sum();
+    dbg!(middles_sum);
+}
 
 fn main() {
     part1();
